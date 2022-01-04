ef4077802a2d981b1d375755b4655bc0ae4e3673
User <shubhamlokare1999@gmail.com>
2022-01-04 17:50:43 +0530

    FirstCommit....

---
+++ src/main/java/GitDmoTest/GitTest/App.java
@@ -0,0 +1,13 @@
+package GitDmoTest.GitTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
